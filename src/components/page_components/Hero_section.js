55efa033255f039b52e5afbdd81fb4f0e8834d5d
vir-webdev <viratgamit1.@gmail.com>
2024-07-31 21:32:05 +0530

    7/31/24(work)

--- src/components/page_components/Hero_section.js
+++ src/components/page_components/Hero_section.js
@@ -2,14 +2,13 @@ import React from 'react'
 import '../../css/hero_section.css'
 export default function Hero_section() {
   return (
-    <div>
-        <section id="hero">
-        <h4>Trade-in-offer</h4>
-        <h2>Super value deals</h2>
-        <h1>On all products</h1>
-        <p>save more coupons & up to 70% off!</p>
-        <button>Shop now</button>
-      </section>
-    </div>
+    <div id="hero">
+    <div class="background"></div>
+    <h4>Trade-in-offer</h4>
+    <h2>Super value deals</h2>
+    <h1>On all products</h1>
+    <p>save more coupons & up to 70% off!</p>
+    <button>Shop now</button>
+  </div>
   )
 }
